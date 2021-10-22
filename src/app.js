30680a3757b6cebcf235cd09fb3a07812ba4e38d
AbdullahSalihOner <salihriyad@hotmail.com>
2021-10-22 19:01:19 +0300

    firstCommit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+const euroDun = 11;
+console.log(euroDun);
+
+let konurKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log(konurKredileri);
+
+console.log("<ul>");
+for (let i = 0; i < 3; i++) {
+    console.log("<li>"+konurKredileri[1]+"</li>")
+    
+}
+console.log("</ul>");
